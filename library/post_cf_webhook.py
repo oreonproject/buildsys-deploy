d01a08226a9ccc42ef5183007bf487943b99d6be
Brandon Lester <blester@localhost.localdomain>
2025-08-03 14:28:59 -0700

    fixes

--- library/post_cf_webhook.py
+++
@@ -1,8 +0,0 @@
-data = {
-    "job_id":   task['id'],
-    "srpm":     task['params'][0],
-    "arch":     task['arch'],          # Koji passes this in every build task
-    "oreon":    task['tag_name'],      # or parse from build target
-    "callback": f"{ALBS_API}/internal/task/{task['id']}/done"
-}
-requests.post(os.environ["CF_ALBS_WEBHOOK"], json=data, timeout=10)
